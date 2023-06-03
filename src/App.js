34c84f87c27d4feefd287a6f114e543829b79da4
w3App <kshatriyakabawat@gmail.com>
2023-06-03 16:16:44 +0530

    upload tk-admin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import io from 'socket.io-client'
+import Aside from './page/aside'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RecentWork from './page/recent-work'
+import CaseStudies from './page/case-studie'
+import ParticularCaseStudie from './page/case-studie/particular-case-studie'
 const url = 'https://socket-server-mwbe.onrender.com'
-
 const App = () => {
   const socket = io(url)
   const data = {
@@ -14,8 +18,21 @@ const App = () => {
     })
   })
   return (
-    <div>
-
+    <div className='main_container'>
+      <div className="main_container_inner">
+        <BrowserRouter>
+          <div className="aside">
+            <Aside />
+          </div>
+          <div className="main">
+            <Routes>
+              <Route path='/recent-work' element={<RecentWork />} />
+              <Route path='/case-studies' element={<CaseStudies />} />
+              <Route path='/particular-case-studie' element={<ParticularCaseStudie />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </div>
   )
 }
---
+++ src/domain.js
@@ -0,0 +1,4 @@
+// const BaseURL = 'https://admin-server-topaz.vercel.app'
+// const BaseURL = 'http://localhost:2917'
+const BaseURL = 'https://socket-server-mwbe.onrender.com'
+export default BaseURL
