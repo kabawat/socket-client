4bf91b038caa67f2eb3691ab07a80d6fae83a6e6
w3App <kshatriyakabawat@gmail.com>
2023-06-04 12:05:09 +0530

    Update domain.js

--- src/domain.js
+++ src/domain.js
@@ -1,4 +1,5 @@
 // const BaseURL = 'https://admin-server-topaz.vercel.app'
 // const BaseURL = 'http://localhost:2917'
-const BaseURL = 'https://socket-server-mwbe.onrender.com'
+// const BaseURL = 'https://socket-server-mwbe.onrender.com'
+const BaseURL = 'https://tkadmin.vercel.app'
 export default BaseURL
